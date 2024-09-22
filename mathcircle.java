4d035917576272e0438cf36f2fe2721c3d1a3fa2
rupal <roopalsatija@gmail.com>
2024-09-22 23:56:24 +0530

    my java code till 21-09

---
+++ mathcircle.java
@@ -0,0 +1,15 @@
+package addition;
+
+public class mathcircle {
+ static double pi=Math.PI;
+static double area(int r)
+{
+	return pi*Math.pow(r, 2);
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("area is"+ area(8));
+	}
+
+}
